99124c4e2dee99f1fb39936a0fe6a1e5811def0d
nicolassg96 <83722269+nicolassg96@users.noreply.github.com>
2021-06-09 10:26:49 -0500

    Completed module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -7,6 +7,8 @@ using StarChart.Data;
 
 namespace StarChart.Controllers
 {
+    [Route("")]
+    [ApiController]
     public class CelestialObjectController: ControllerBase
     {
         private readonly ApplicationDbContext _context;
